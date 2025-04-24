8016b1ee62a07b44a483bf4b559e7c638f4889e7
Akasaka Ryuunosuke <local@local.local>
2025-04-24 22:59:16 +0900

    Add default radio stations

--- firm/src/radio_store.cpp
+++ firm/src/radio_store.cpp
@@ -1,13 +1,23 @@
 #include <radio_store.h>
 #include <esper-core/prefs.h>
+#include <vector>
 
 namespace NetRadio {
+    static const std::vector<std::pair<std::string, std::string>> default_radios = { 
+        {"Happy Hardcore", "u1.happyhardcore.com"},
+        {"Provoda.ch", "station.waveradio.org/provodach"},
+        {"Sovietwave", "station.waveradio.org/soviet"},
+        {"Nightwave Plaza", "radio.plaza.one/mp3"},
+        {"Keygen FM", "stream.keygen-fm.ru:8082"},
+        {"Radio Cafe", "on.radio-cafe.ru:6045"},
+    };
+
     const Prefs::Key<std::string> NamePrefsKeyForIndex(int idx) {
-        return {"netr_n"+std::to_string(idx), ""};
+        return {"netr_n"+std::to_string(idx), default_radios[idx].first};
     }
 
     const Prefs::Key<std::string> URLPrefsKeyForIndex(int idx) {
-        return {"netr_u"+std::to_string(idx), ""};
+        return {"netr_u"+std::to_string(idx), default_radios[idx].second};
     }
 
     const std::string URLForIndex(int idx) {
